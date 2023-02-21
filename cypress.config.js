e259a8d0fae126ca8dee5ce66493763ef34e69bf
Jairo Bermudez <jairo.bermudez@afex.cl>
2023-02-21 12:02:34 -0500

    se actualiza libererias

--- cypress.config.js
+++ cypress.config.js
@@ -21,22 +21,7 @@ module.exports = defineConfig({
     API_CLIENTS_VALUE:'9RubKS6Sir1pNh1Mv7Zhp8JDxIoOkn0VnSKjQs25',
     API_CLIENTS:'https://he9n83rsg0.execute-api.us-west-2.amazonaws.com/staging',
     API_TRANSFERS_VALUE:'l48r2vlY0B9BdzsHzym6u3uxnHLug6023pcW203b',
-    SLACK_WEBHOOK:'https://hooks.slack.com/services/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'
+    SLACK_WEBHOOK:'https://hooks.slack.com/services/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',
+    SLACK_TOKEN:'xoxb-671745439778-4820436549719-Q7AS9e1N1XqF2OpsSi68rSDH'
   }
 });
-
-
-/*const {IncomingWebhook } = require('@slack/webhook');
-
-const url = "https://hooks.slack.com/services/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";
-const webhook = new IncomingWebhook(url);
-
-(async () => {
-  const { totalTests, totalPassed, totalFailed } = Cypress.runner.stats;
-
-  await webhook.send({
-    text: `Cypress tests summary:\n- Total tests: ${totalTests}\n- Passed tests: ${totalPassed}\n- Failed tests: ${totalFailed}`
-  });
-  url.send(message);
-  });
-*/
