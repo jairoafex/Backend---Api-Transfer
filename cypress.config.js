2c201c5bcf2a52427423f464e84d20c8cbbe3dbb
jairoafex <118744038+jairoafex@users.noreply.github.com>
2023-02-20 21:49:09 -0500

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -28,7 +28,7 @@ module.exports = defineConfig({
 
 const {IncomingWebhook } = require('@slack/webhook');
 
-const url = "https://hooks.slack.com/services/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";
+const url = "https://hooks.slack.com/services/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";
 const webhook = new IncomingWebhook(url);
 
 (async () => {
